ae27f8d6969958c3a6fe6a967e6be25af568c04f
travail <travail@macbook-pro-de-frederic.home>
2020-04-28 02:54:02 +0200

    nombreux essais tests

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ tests/TaskControllerTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TaskControllerTest extends WebTestCase
+{
+    public function testTasksListResponse()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/tasks');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testNoTaskWhenStart()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/tasks');
+
+        $nbElements = $crawler->filter('div.alert-warning')->count();
+
+        $this->assertEquals($nbElements, 1);
+    }
+
+    public function testTaskSeenWhenCreated()
+    {
+        $clientPost = static::createClient(
+            [],
+            [
+                'HTTPS' => true
+            ]
+        );
+
+        // $clientGet = clone $clientPost;
+        
+        $crawlerPost = $clientPost->request(
+            'POST',
+            '/tasks/create',
+            [
+                "content" => "première tache contenu",
+                "title" => "première tache titre"
+            ]
+        );
+
+        // $nbDivSuccess = $crawlerPost->filter('div.alert-success')->count();
+        // $nbPDansList = $crawlerPost->filter('p#dansList')->count();
+        // $nbPDansCreate = $crawlerPost->filter('p#dansCreate')->count();
+
+        // $this->assertEquals($nbDivSuccess, 1);
+        // $this->assertEquals($nbPDansList, 1);
+        // $this->assertEquals($nbPDansCreate, 0);
+
+        $this->assertResponseRedirect('/tasks');
+
+        // $clientGet = static::createClient(
+        //     [],
+        //     [
+        //         'HTTPS' => true
+        //     ]
+        // );
+
+        // $crawlerGet = $clientGet->request(
+        //     'GET',
+        //     '/tasks'
+        // );
+
+        // $nbAlerts = $crawlerGet->filter('div.alert-warning')->count();
+
+        $nbAlerts = $crawlerPost->filter('div.alert-warning')->count();
+
+        $this->assertEquals($nbAlerts, 0);
+    }
+
+    // public function testTaskSeenWhenCreated()
+    // {
+    //     $client = static::createClient(
+    //         [],
+    //         [
+    //             'HTTPS' => true
+    //         ]
+    //     );
+
+    //     $crawler = $client->request('GET', '/tasks/create');
+
+    //     $form = $crawler->selectButton('Ajouter')->form([
+    //         'title' => 'titre première tache',
+    //         'content' => 'contenu première tache'
+    //     ]);
+
+    //     $client->submit($form);
+
+    //     $this->assertResponseRedirect('/login');
+
+    //     $client->followRedirect();
+
+    //     $this->assertSelectorExists('p#dansList');
+    // }
+    
+}
