98a50f47a2a7c630186b573622d8cdf3e78a45d4
travail <travail@macbook-pro-de-frederic-1.home>
2020-04-29 15:00:24 +0200

    avant rdv

--- tests/TaskControllerTest.php
+++ tests/TaskControllerTest.php
@@ -8,7 +8,7 @@ class TaskControllerTest extends WebTestCase
 {
     public function testTasksListResponse()
     {
-        $client = static::createClient();
+        $client = static::createClient([], ['HTTPS' => true]);
         $crawler = $client->request('GET', '/tasks');
 
         $this->assertResponseIsSuccessful();
@@ -16,7 +16,12 @@ class TaskControllerTest extends WebTestCase
 
     public function testNoTaskWhenStart()
     {
-        $client = static::createClient();
+        $client = static::createClient(
+            [],
+            [
+                'HTTPS' => true
+            ]
+        );
         $crawler = $client->request('GET', '/tasks');
 
         $nbElements = $crawler->filter('div.alert-warning')->count();
@@ -26,81 +31,27 @@ class TaskControllerTest extends WebTestCase
 
     public function testTaskSeenWhenCreated()
     {
-        $clientPost = static::createClient(
+        $client = static::createClient(
             [],
             [
                 'HTTPS' => true
             ]
         );
 
-        // $clientGet = clone $clientPost;
+        $crawler = $client->request('GET', '/tasks/create');
 
-        $csrfToken = $clientPost->getContainer()->get('security.csrf.token_manager')->getToken('task'); // pas sur de la string dans getToken
-        
-        $crawlerPost = $clientPost->request(
-            'POST',
-            '/tasks/create',
-            [
-                'csrf_token' => $csrfToken,
-                "content" => "première tache contenu",
-                "title" => "première tache titre"
-            ]
-        );
+        $form = $crawler->selectButton('Ajouter')->form([
+            'task[title]' => 'titre première tache',
+            'task[content]' => 'contenu première tache'
+        ]);
 
-        $this->assertResponseRedirects();
+        $client->submit($form);
 
-        // $nbDivSuccess = $crawlerPost->filter('div.alert-success')->count();
-        // $nbPDansList = $crawlerPost->filter('p#dansList')->count();
-        // $nbPDansCreate = $crawlerPost->filter('p#dansCreate')->count();
+        $client->followRedirect();
 
-        // $this->assertEquals($nbDivSuccess, 1);
-        // $this->assertEquals($nbPDansList, 1);
-        // $this->assertEquals($nbPDansCreate, 0);
-
-        // $this->assertResponseRedirects('/tasks');
-
-        // $clientGet = static::createClient(
-        //     [],
-        //     [
-        //         'HTTPS' => true
-        //     ]
-        // );
-
-        // $crawlerGet = $clientGet->request(
-        //     'GET',
-        //     '/tasks'
-        // );
-
-        // $nbAlerts = $crawlerGet->filter('div.alert-warning')->count();
-
-        // $nbAlerts = $crawlerPost->filter('div.alert-warning')->count();
-
-        // $this->assertEquals($nbAlerts, 0);
+        $this->assertSelectorExists('p#dansListe');
     }
 
-    // public function testTaskSeenWhenCreated()
-    // {
-    //     $client = static::createClient(
-    //         [],
-    //         [
-    //             'HTTPS' => true
-    //         ]
-    //     );
-
-    //     $crawler = $client->request('GET', '/tasks/create');
-
-    //     $form = $crawler->selectButton('Ajouter')->form([
-    //         'title' => 'titre première tache',
-    //         'content' => 'contenu première tache'
-    //     ]);
-
-    //     $client->submit($form);
-
-    //     $this->assertResponseRedirect('/login');
-
-    //     $client->followRedirect();
-
-    //     $this->assertSelectorExists('p#dansList');
-    // }
+    //     // $nbDivSuccess = $crawlerPost->filter('div.alert-success')->count();
     
 }
