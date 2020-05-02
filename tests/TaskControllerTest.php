8551961440bdea3cd0c7c89a3e7ae308435b7e6e
travail <travail@macbook-pro-de-frederic.home>
2020-05-02 22:18:43 +0200

    premier --coverage-html réussi

--- tests/TaskControllerTest.php
+++ tests/TaskControllerTest.php
@@ -2,27 +2,36 @@
 
 namespace App\Tests;
 
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TaskControllerTest extends WebTestCase
 {
+    private $client;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = static::createClient([], ['HTTPS' => true]);
+
+        /** @var EntityManagerInterface $em */
+        $em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+
+        $purger = new ORMPurger($em);
+        $purger->purge();
+    }
+
     public function testTasksListResponse()
     {
-        $client = static::createClient([], ['HTTPS' => true]);
-        $crawler = $client->request('GET', '/tasks');
+        $crawler = $this->client->request('GET', '/tasks');
 
         $this->assertResponseIsSuccessful();
     }
 
     public function testNoTaskWhenStart()
     {
-        $client = static::createClient(
-            [],
-            [
-                'HTTPS' => true
-            ]
-        );
-        $crawler = $client->request('GET', '/tasks');
+        $crawler = $this->client->request('GET', '/tasks');
 
         $nbElements = $crawler->filter('div.alert-warning')->count();
 
@@ -31,23 +40,18 @@ class TaskControllerTest extends WebTestCase
 
     public function testTaskSeenWhenCreated()
     {
-        $client = static::createClient(
-            [],
-            [
-                'HTTPS' => true
-            ]
-        );
-
-        $crawler = $client->request('GET', '/tasks/create');
+        $crawler = $this->client->request('GET', '/tasks/create');
 
         $form = $crawler->selectButton('Ajouter')->form([
             'task[title]' => 'titre première tache',
             'task[content]' => 'contenu première tache'
         ]);
 
-        $client->submit($form);
+        $this->client->submit($form);
 
-        $client->followRedirect();
+        $this->client->followRedirect();
+
+        //assertredirect
 
         $this->assertSelectorExists('p#dansListe');
     }
