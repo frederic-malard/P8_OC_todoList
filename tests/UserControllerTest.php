9a6239ecc3bf8c65c104cc15450ab3e7cc9b0a5f
travail <travail@macbook-pro-de-frederic.home>
2020-05-03 15:37:31 +0200

    migrations excluded from tests

---
+++ tests/UserControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+        
+}
