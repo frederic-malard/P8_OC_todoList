66f9ea75ea5bdbaa997b7ec6ae2c0094bad60e7d
travail <travail@macbook-pro-de-frederic.home>
2020-06-23 22:21:24 +0200

    correction usertype constraints

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -22,7 +25,22 @@ class UserType extends AbstractType
                 'required' => true,
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
-                'label' => 'mot de passe'
+                'label' => 'mot de passe',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe devrait faire au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                    new Regex([
+                        'pattern' => '/(^[0-9]+[a-zA-Z]+[0-9a-zA-Z]*$)|(^[a-zA-Z]+[0-9]+[0-9a-zA-Z]*$)/',
+                        'message' => 'Votre mot de passe doit être constitué uniquement de chiffres et lettres (au moins un de chaque), doit contenir au moins un chiffre, et au moins une lettre.'
+                    ])
+                ]
             ])
             ->add('email', EmailType::class, ['label' => 'Adresse email'])
             ->add('roles', ChoiceType::class, [
